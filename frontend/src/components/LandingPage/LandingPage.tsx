0a2ba5ec2b17b22504314aa63c6f6feb278a360e
passid23 <passid23@htl-kaindorf.at>
2026-04-10 16:33:50 +0200

    add: clean react working environment

---
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import './LandingPage.css'
+
+
+function LandingPage(){
+    return(
+        <div>
+            <h1 className="red">Welcome to Selfinity Lab</h1>
+        </div>
+    )
+}
+
+export default LandingPage
