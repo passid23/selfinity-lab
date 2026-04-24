5efa4a927643120aebf4ca0947d5d50e6e34afa1
MRespondek1 <Resmod23@htl-kaindorf.at>
2026-04-24 12:04:28 +0200

    add courses

--- backend/models/Course.ts
+++ backend/models/Course.ts
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
+import { Router, type Request, type Response } from 'express';
 
 const CourseSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    category: { type: String, required: true }, // 'finance', 'mental_health', etc.
-    minPoints: { type: Number, required: true }, // z.B. 0
-    maxPoints: { type: Number, required: true }, // z.B. 10
+    title:       { type: String, required: true },
+    category:    { type: String, required: true },
+    minPoints:   { type: Number, required: true },
+    maxPoints:   { type: Number, required: true },
     description: String,
-    link: String
+    link:        String
 });
 
 export const Course = mongoose.model('Course', CourseSchema);
+
+
+
+
